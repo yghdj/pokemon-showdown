eccfcbbd2be6815fb3208859836f981f2bb135ad
Luc Moussiegt <luc.moussiegt@gmail.com>
2025-05-19 23:33:31 +0200

    Add new custom format

---
+++ config/custom-formats.ts
@@ -0,0 +1,16 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: import('../sim/dex-formats').FormatList = [
+	{
+		section: "Pojemo",
+	},
+	{
+		name: "[Gen 9] Pojemo OU",
+		desc: `Mechanics and Pok&eacute;mon sets modifiedd by the NDPS group chat.`,
+		mod: 'pojemo',
+		ruleset: ['Standard'],
+		rated: false
+	},
+];
+
