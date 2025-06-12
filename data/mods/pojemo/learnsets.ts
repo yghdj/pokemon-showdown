d7d1edbdd8770c1141eb0c5edeefbf728733e9aa
Runj <paul.rungette@gmail.com>
2025-06-12 19:26:19 +0200

    first try fixing mightyena lmao

---
+++ data/mods/pojemo/learnsets.ts
@@ -0,0 +1,11 @@
+export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTable = {
+    /* go mettre tout en 9L1 */
+    mightyena: {
+        inherit: true,
+		learnset: {
+            bonemerang: ["9L1"],
+			bulkup: ["9L1"],
+            moonlight: ["9L1"],
+        },
+	},
+}
--- data/mods/pojemo/pokedex.ts
+++ data/mods/pojemo/pokedex.ts
@@ -2,5 +2,14 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 	magneton: {
 		inherit: true,
 		types: ["Electric", "Fire"],
-	}
+	},
+	meganium: {
+		inherit: true,
+		types: ["Grass", "Dragon"],
+	},
+	mightyena: {
+		inherit: true,
+		baseStats: { hp: 70, atk: 100, def: 80, spa: 70, spd: 70, spe: 70 },
+		abilities: { 0: "Intimidate", 1: "Defiant", H: "Moxie" },
+	},
 }
